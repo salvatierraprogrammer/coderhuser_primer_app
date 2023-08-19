e63fc79837636c299ff3b39cdb283fc2acd85023
Diego <123164956+salvatierraprogrammer@users.noreply.github.com>
2023-08-18 22:24:19 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text >Hola Coder!</Text>
+      <Text >Aplicacion Inicio</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f0f0',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
+ 
 });
